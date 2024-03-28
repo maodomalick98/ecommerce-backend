625af63cad1042e4dc2a5366fe42b99a990eecca
maodomalick98 <maodomalick.gaye@epsi.fr>
2024-03-28 20:54:11 +0100

    add products searching endpoint, add loombokk annotation

--- src/main/java/com/malick/ecommerce/config/SpringDataRestConfig.java
+++ src/main/java/com/malick/ecommerce/config/SpringDataRestConfig.java
@@ -2,14 +2,27 @@ package com.malick.ecommerce.config;
 
 import com.malick.ecommerce.entity.Product;
 import com.malick.ecommerce.entity.ProductCategory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
-public class MyDataRestConfig implements RepositoryRestConfigurer {
+public class SpringDataRestConfig implements RepositoryRestConfigurer {
+
+    private final EntityManager entityManager;
+    @Autowired
+    public SpringDataRestConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
@@ -24,5 +37,16 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(disabledMethods
                 )).withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(disabledMethods));
+        exposeEntityIds(config);
+    }
+
+    private void exposeEntityIds(RepositoryRestConfiguration config) {
+        Set<EntityType<?>> entities = this.entityManager.getMetamodel().getEntities();
+        List<Class> entityClasses = new ArrayList<>();
+        for(EntityType entityType: entities) {
+            entityClasses.add(entityType.getJavaType());
+        }
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
     }
 }
--- src/main/java/com/malick/ecommerce/entity/ProductCategory.java
+++ src/main/java/com/malick/ecommerce/entity/ProductCategory.java
@@ -1,11 +1,13 @@
 package com.malick.ecommerce.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.util.Set;
 
 @Entity
 @Table(name = "product_category")
+@Data
 public class ProductCategory {
 
     @Id
@@ -17,20 +19,4 @@ public class ProductCategory {
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
     private Set<Product> products;
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
-
-    public Set<Product> getProducts() {
-        return products;
-    }
-
-    public void setProducts(Set<Product> products) {
-        this.products = products;
-    }
 }
